2823bdf02bc6ba7f47d79ecab128d74f344ee983
mgirl-labs <mgirl-labs@users.noreply.github.com>
2026-01-16 10:54:33 +0900

    add SDK barrel export (#20)

---
+++ sdk/src/index.ts
@@ -0,0 +1,11 @@
+export { CPEClient } from "./client";
+export { ProofVerifier } from "./verifier";
+export { BundleBuilder } from "./builder";
+export type {
+  BundleConfig,
+  BundleResult,
+  CpeBundle,
+  VerificationResult,
+  ConflictInfo,
+  EpochData,
+} from "./types";
